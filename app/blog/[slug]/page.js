8f0c03273c739476348b6ed81fbd8a62cb25c017
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-06-23 15:59:03 -0500

    created next js folder structure

---
+++ app/blog/[slug]/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogPostPage = ({params}) => {
+  return (
+   <main>
+    <h1>Blog Post</h1>
+    <p>{params.slug}</p>
+   </main>
+  )
+}
+
+export default BlogPostPage
