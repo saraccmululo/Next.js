59b7814c5a73c41cc8e14fae36f17878f698055b
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-06-25 16:05:02 -0500

    created loading component

---
+++ Foodies-app/app/meals/loading-out.js
@@ -0,0 +1,9 @@
+import styles from './loading.module.css'
+
+const MealsLoadingPage = () => {
+  return (
+    <p className={styles.loading}>Getting meals...</p>
+  )
+}
+
+export default MealsLoadingPage
