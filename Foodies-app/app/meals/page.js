1623c6f6ec795ed6cab83fabe33cef02ffd0904e
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-06-25 12:11:44 -0500

    created the Meals page component

--- Foodies-app/app/meals/page.js
+++ Foodies-app/app/meals/page.js
@@ -1,13 +1,27 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import styles from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
 const MealsPage = () => {
   return (
     <>
-    <h1>List of Meals</h1>
-    
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created {""}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}></main>
+      <MealsGrid meals={[]}/>
     </>
-  )
-}
+  );
+};
 
-export default MealsPage
+export default MealsPage;
