a40baa5d781c85b444277c80c7272a03dd3bcf58
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-06-30 14:31:46 -0500

    fixed fallback

--- PageRouter/MeetUp-app/.next/static/chunks/webpack.js
+++ PageRouter/MeetUp-app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7303060edfc819a0"; }
+/******/ 		__webpack_require__.h = function() { return "aae09e481faec575"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
